4856f91c44ae4e6895da45b16f4a107d5d1d8ab0
Zaid Khan <zaidkhan5548@gmail.com>
2022-04-11 12:55:53 +0530

    update message value from application.properties

--- SpringBootApplicationProperties/src/main/java/com/khan/org/controller/MyController.java
+++ SpringBootApplicationProperties/src/main/java/com/khan/org/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.khan.org.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+	@Value("${my.college.name}")
+	private String myCollegeName;
+	
 	@RequestMapping(path = "/",method = RequestMethod.GET)
 	public String welcome() {
-		return "<h1>Welcome To Application Properties Spring Boot Project</h1>";
+		return "<h1>Welcome To Application Properties Spring Boot Project</h1>"+myCollegeName;
 	}
 	
 }
