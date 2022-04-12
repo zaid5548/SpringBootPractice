a084ad5f3b37a6d025af6351f9bcf76382feaf8e
Zaid Khan <zaidkhan5548@gmail.com>
2022-04-12 16:04:33 +0530

    SpringBoot + JPA Exception Handling

--- SpringBootWithH2Database/src/main/java/com/khan/org/service/UserService.java
+++ SpringBootWithH2Database/src/main/java/com/khan/org/service/UserService.java
@@ -17,63 +17,78 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
-	public void updateUserAgeById(int id,int newAge) {
-		System.out.println("Finding Entiti by Id = "+id);
-		Optional<User> findById=getUserRepository().findById(id);
-		User user=findById.get();
-		user.setAge(newAge);
-		userRepository.save(user);
+
+	public void updateUserAgeById(Integer id, Integer newAge) throws UserDataAccessException {
+		System.out.println("Updating Entity by Id = " + id);
+		if (id == null)
+			throw new UserDataAccessException("User Id can't be null");
+		else if (newAge == null)
+			throw new UserDataAccessException("User Age can't be null");
+		Optional<User> findById = getUserRepository().findById(id);
+		if(findById.isPresent()) {	
+			User user = findById.get();
+			user.setAge(newAge);
+			userRepository.save(user);
+		}else {
+			throw new UserDataAccessException("User With ID =" + id + " Not found in database");
+		}
 	}
-	
+
 	public void createUsers() {
-		User user1=new User();
+		User user1 = new User();
 		user1.setName("Zaid Khan");
 		user1.setAge(22);
-		user1.setDob(LocalDate.of(1997,Month.APRIL,20));
-		
-		User user2=new User();
+		user1.setDob(LocalDate.of(1997, Month.APRIL, 20));
+
+		User user2 = new User();
 		user2.setName("Ajinkya Rahne");
 		user2.setAge(33);
-		user2.setDob(LocalDate.of(1989,Month.DECEMBER,9));
-		
-		User user3=new User();
+		user2.setDob(LocalDate.of(1989, Month.DECEMBER, 9));
+
+		User user3 = new User();
 		user3.setName("Rohit Sharma");
 		user3.setAge(28);
-		user3.setDob(LocalDate.of(1994,Month.JUNE,23));
-		
-		List<User> userList=new ArrayList<>();
-		
+		user3.setDob(LocalDate.of(1994, Month.JUNE, 23));
+
+		List<User> userList = new ArrayList<>();
+
 		userList.add(user1);
 		userList.add(user2);
 		userList.add(user3);
-		
-		Iterable<User> dbSaveduser=getUserRepository().saveAll(userList);
+
+		Iterable<User> dbSaveduser = getUserRepository().saveAll(userList);
 		System.out.println("Following users info saved in databse: ");
 		for (User user : dbSaveduser) {
 			System.out.println(user);
 		}
-		
+
 	}
 
 	public void crateUser() {
-		User user=new User();
+		User user = new User();
 		user.setName("Zaid Khan");
 		user.setAge(22);
-		user.setDob(LocalDate.of(1998,Month.APRIL,20));
+		user.setDob(LocalDate.of(1998, Month.APRIL, 20));
 		User dbUser = getUserRepository().save(user);
 		System.out.println("Following user info saved in databse: ");
 		System.out.println(dbUser);
 	}
 
-	public Optional<User> findUserById(int id) {
-		return getUserRepository().findById(id);
+	public User findUserById(Integer id) throws UserDataAccessException {
+		System.out.println("Finding Entity by Id : " + id);
+		if (id == null)
+			throw new UserDataAccessException("User Id can't be null");
+
+		Optional<User> findById = getUserRepository().findById(id);
+		if (findById.isPresent())
+			return findById.get();
+		throw new UserDataAccessException("User With ID =" + id + " Not found in database");
 	}
-	
-	public Iterable<User> findAllUser(){
+
+	public Iterable<User> findAllUser() {
 		return getUserRepository().findAll();
 	}
-	
+
 	public UserRepository getUserRepository() {
 		return userRepository;
 	}
@@ -82,5 +97,4 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
-	
 }
