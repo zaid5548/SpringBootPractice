8d6f554e57bff35643f60fd269db19d605aecac7
Zaid Khan <zaidkhan5548@gmail.com>
2022-04-15 14:18:31 +0530

    SpringBoot Configuration With XML

--- SpringBootXMLConfiguration/src/main/java/com/khan/org/SpringBootXmlConfigurationApplication.java
+++ SpringBootXMLConfiguration/src/main/java/com/khan/org/SpringBootXmlConfigurationApplication.java
@@ -2,8 +2,10 @@ package com.khan.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:beans.xml"})
 public class SpringBootXmlConfigurationApplication {
 
 	public static void main(String[] args) {
