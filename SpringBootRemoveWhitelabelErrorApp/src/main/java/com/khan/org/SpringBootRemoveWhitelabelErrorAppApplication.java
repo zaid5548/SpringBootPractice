9d0bb716e9b2eb57dc98e8496bc3dc045aab42e6
Zaid Khan <zaidkhan5548@gmail.com>
2022-04-15 13:54:45 +0530

    Create a SpringBootRemoveWhiteLableError project

---
+++ SpringBootRemoveWhitelabelErrorApp/src/main/java/com/khan/org/SpringBootRemoveWhitelabelErrorAppApplication.java
@@ -0,0 +1,13 @@
+package com.khan.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRemoveWhitelabelErrorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRemoveWhitelabelErrorAppApplication.class, args);
+	}
+
+}
